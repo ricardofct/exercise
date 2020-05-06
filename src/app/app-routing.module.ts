bd62d9650859db8c95332d967f5199d0407dcb3c
Ricardo Trindade <ricardo.trindade.ext@spms.min-saude.pt>
2020-05-06 15:36:21 +0100

    - exercise finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'generator'
+  },
+  {
+    path: 'generator',
+    loadChildren: () => import('./generator/generator.module').then(({ GeneratorModule }) => GeneratorModule)
+  },
+  {
+    path: 'payments',
+    loadChildren: () => import('./payments/payments.module').then(({ PaymentsModule }) => PaymentsModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'generator'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,12 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
